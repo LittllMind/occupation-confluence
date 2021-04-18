7a67d7e30334ec46e209117a2c747167e691a328
LittllMind <aurelien.tisserand18@gmail.com>
2021-04-18 20:19:12 +0200

    merde

--- src/view/frontend/acceuil.php
+++ src/view/frontend/acceuil.php
@@ -6,8 +6,49 @@
   <img style:"width: 1000px" src="assets/images/Occupation.jpeg" alt="Occupation">
 </div>
 
-<img style:"width: 1000px" src="https://i.postimg.cc/4dZr3HqH/occupation-confluence.jpg" alt="Occupation">
+<div class="manifest">
+  <strong><h1>Manifeste</h1></strong>
 
+  <p>
+    Nous, Citoyens.nes, Atristes, Intermitent.e.s et permanent.es, Techniciens.nes du spectacle vivant et de l'audiovisuel, Administrateur.ices, Enseignant.e.s artistiques, <br>
+      Étudiant.e.s occuppons depuis Lundi 29 Mars l'Opéra Confluence d'Avignon.
+  </p>
+  <p>
+    Nous remercions toutes les personnes qui nous soutiennent ainsi que l'équipe de l'Opéra Confluence pour sa solidarité avec notre mouvement. <br>
+    Nous sommes déterminés à occuper l'espace intérieur, mais aussi l'espace extérieur, devant l'opéra, dans la rue, de ré-investir l'espace public <br>
+    qui nous a été confisqué, en écho aux occupations nationales, et en résonnance à la FabricA.
+  </p>
+  <p>
+    Ce moment est un temps de préparation, de rédaction et d'organisation pour les jours a venir. <br>
+    Ainsi, nous attendons dès le lundi 29 mars afin de mettre en place et d'inventer ensemble nos modes d'actions, et continuer d'investir les lieux.
+  </p>
+  <p>
+    Nous appelons toutes les personnes solidaires à ce mouvement, à nous rejoindre.<br>
+    Nous continuerons à revendiquer la fin d'une politique de précarisation du travail,
+    l'accès à la culture pour tous.te.s, et l'ouverture des lieux culturels et de transmission du savoir.
+  </p>
+
+  <h1>Nous demandons</h1>
+  <ul>
+    <li>
+      L'abondon pur et simple de la réforme de l'assurance chomage. <br>
+      Avec cette réforme, c'est 840 000 intermitent.e.s de l'emploi qui perdront, en moyenne, 24% de leurs revenus.
+    </li>
+    <li>
+      Plus de moyens pour les hopitaux : moins de fermetures, plus d'ouvertures de lits.
+    </li>
+    <li>
+      La revalorisation des salaires du personnel soignant.
+    </li>
+    <li>
+      L'élargissement du numérus clausus : seuil d'admission aux études de santé françaises.
+    </li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+  </ul>
+</div>
 
 
 <?php $content = ob_get_clean(); ?>
--- src/view/frontend/template.php
+++ src/view/frontend/template.php
@@ -30,17 +30,33 @@
 
       <div class="navbar">
         <img src="assets/images/logo-occupation.webp" alt="Occupation">
+        <div class="container">
+          <div class="row">
+            <br/><br/>
+            <div class="col-md-12">
+
+
+    </div>
+
+
+</div>
+</div>
+
         <div class="navbar-links">
           <div class="posts-list">
              <button type="button" name="button" class="btn"> <a href="index.php?action=index">HOME</a>  </button>
              <button type="button" name="button" class="btn"> <a href="index.php?action=listPosts">BLOG</a>  </button>
              <button type="button" name="button" class="btn"> <a href="index.php?action=listPosts">GALERIE</a>  </button>
+
              <!-- <a href="index.php?action=postForm">Add Blog Post</a> -->
 
           </div>
         </div>
 
       </div>
+      <a href="http://scripteden.com/download/eden-ui/" target="_blank" class="btn-social btn-facebook"><i class="fa fa-facebook"></i></a>
+      <a href="http://scripteden.com/download/eden-ui/" target="_blank" class="btn-social btn-instagram"><i class="fa fa-instagram"></i></a>
+      <a href="http://scripteden.com/download/eden-ui/" target="_blank" class="btn-social btn-twitter"><i class="fa fa-twitter"></i></a>
       <?= $content ?>
       <footer>
 
