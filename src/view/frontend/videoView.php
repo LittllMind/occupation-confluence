75083336053e4f50e0c2d06f8f71e0c17a4bebd1
LittllMind <aurelien.tisserand18@gmail.com>
2021-04-22 16:46:33 +0200

    starting video page

---
+++ src/view/frontend/videoView.php
@@ -0,0 +1,39 @@
+<?php $title = 'Occupation confluence'; ?>
+
+<?php ob_start(); ?>
+<?php
+while ($data = $posts->fetch()) {
+    ?>
+    <div class="news">
+        <div class="news-top">
+          <h3>
+            <?= htmlspecialchars($data['title']); ?>
+
+          </h3>
+        </div>
+        <a href="index.php?action=post&amp;id=<?= $data['id'] ?>">
+        <img src="<?= $data['image_url']?>" alt="image">
+        </a>
+        <div class="post-content">
+
+          <p>
+              <?=nl2br(htmlspecialchars($data['content']));?>
+          </p>
+
+        </div>
+        <div class="news-footer">
+          <em>le <?= $data['creation_date_fr']; ?></em>
+
+          <em>  </em>
+
+        </div>
+
+    </div>
+
+    <?php
+}
+    $posts->closeCursor();
+?>
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php');
