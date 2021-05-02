9ef932d4d9f659c2ec425007efc5204931a4fa55
LittllMind <aurelien.tisserand18@gmail.com>
2021-05-02 12:22:49 +0200

    before merging master

--- src/model/BddManager.php
+++ src/model/BddManager.php
@@ -8,26 +8,26 @@ class BddManager
 
     // Developpement
 
-    protected function dbConnect()
-    {
-        $bdd = new PDO(
-            'mysql:host=localhost;dbname=blog;charset=utf8',
-            'phpLittllMind',
-            'jadore18@P',
-            array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
-        );
-        return $bdd;
-    }
-
-    // Prod
     // protected function dbConnect()
     // {
     //     $bdd = new PDO(
-    //         'mysql:host=occupadlittllmin.mysql.db;dbname=occupadlittllmin;charset=utf8',
-    //         'occupadlittllmin',
-    //         'jadore18O',
+    //         'mysql:host=localhost;dbname=blog;charset=utf8',
+    //         'phpLittllMind',
+    //         'jadore18@P',
     //         array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
     //     );
     //     return $bdd;
     // }
+
+    // Prod
+    protected function dbConnect()
+    {
+        $bdd = new PDO(
+            'mysql:host=occupadlittllmin.mysql.db;dbname=occupadlittllmin;charset=utf8',
+            'occupadlittllmin',
+            'jadore18O',
+            array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
+        );
+        return $bdd;
+    }
 }
